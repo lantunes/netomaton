61d8170473abdecd38c08ea50fe93e54344aa550
Luis Antunes <lantunes@gmail.com>
2018-12-13 14:40:55 -0500

    minor formatting change

--- demos/ca_density_classification/ca_density_classification_demo.py
+++ demos/ca_density_classification/ca_density_classification_demo.py
@@ -12,6 +12,6 @@ rule_number = 6667021275756174439087127638698866559
 print("density of 1s: %s" % (np.count_nonzero(initial_conditions) / 149))
 
 activities, connectivities = evolve(adjacencies, initial_conditions, timesteps=149,
-                                activity_rule=lambda n, c, t: ActivityRule.binary_ca_rule(n, c, rule_number))
+                                    activity_rule=lambda n, c, t: ActivityRule.binary_ca_rule(n, c, rule_number))
 
 plot_grid(activities)
