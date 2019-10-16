2915da1524982f9132b01ed88c8c1442d4995c38
Luis Antunes <lantunes@gmail.com>
2019-10-16 19:13:09 -0400

    adding tests

---
+++ tests/test_hex_evolution.py
@@ -0,0 +1,16 @@
+import netomaton as ntm
+from .rule_test import *
+
+
+class TestHexEvolution(RuleTest):
+
+    def test_snowflake(self):
+        adjacency_matrix = ntm.network.cellular_automaton2d(60, 60, r=1, neighbourhood="Hex")
+
+        initial_conditions = ntm.init_simple2d(60, 60)
+
+        activities, _ = ntm.evolve(initial_conditions, adjacency_matrix, timesteps=31,
+                                   activity_rule=lambda ctx: 1 if sum(ctx.activities) == 1 else ctx.current_activity)
+
+        expected = self._convert_to_list_of_lists("snowflake.ca")
+        np.testing.assert_equal(expected, activities.tolist())
