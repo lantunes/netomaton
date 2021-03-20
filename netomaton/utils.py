335cfd3604a607cbd044679f81a41ee2a3f258e5
Luis Antunes <lantunes@gmail.com>
2021-03-20 00:59:25 -0400

    adding options to animate_network

--- netomaton/utils.py
+++ netomaton/utils.py
@@ -125,7 +125,8 @@ def plot_network(adjacency_matrix):
     plt.show()
 
 
-def animate_network(adjacency_matrices, save=False, interval=50, dpi=80, layout="shell", with_labels=True):
+def animate_network(adjacency_matrices, save=False, interval=50, dpi=80, layout="shell",
+                    with_labels=True, node_color="b", node_size=30):
     fig, ax = plt.subplots()
 
     def update(adjacency_matrix):
@@ -140,9 +141,9 @@ def animate_network(adjacency_matrices, save=False, interval=50, dpi=80, layout=
                     G.add_edge(row_index, node_index)
 
         if layout == "shell":
-            nx.draw_shell(G, with_labels=with_labels)
+            nx.draw_shell(G, with_labels=with_labels, node_color=node_color, node_size=node_size)
         elif layout == "spring":
-            nx.draw_spring(G, with_labels=with_labels)
+            nx.draw_spring(G, with_labels=with_labels, node_color=node_color, node_size=node_size)
         else:
             raise Exception("unsupported layout: %s" % layout)
 
