fef60a787d031c9c7b1eb4ff990f7c12145579ef
Luis Antunes <lantunes@gmail.com>
2021-08-01 09:08:30 -0400

    loosening dependency requirements

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 packages = find_packages(exclude=("tests", "demos", "doc", "resources",))
 
 setup(name="netomaton",
-      version="1.1.0",
+      version="1.1.1",
       description="Netomaton, A Python library for working with Network Automata.",
       long_description="Netomaton is a Python framework for exploring discrete dynamical network systems, "
                        "also known as Network Automata. It is a software abstraction meant to aid in the "
                        "implementation of models of collective computation",
       license="Apache License 2.0",
       classifiers=[
-            'Development Status :: 3 - Alpha',
+            'Development Status :: 5 - Production/Stable',
             'Intended Audience :: Developers',
             'Topic :: Scientific/Engineering :: Artificial Intelligence',
             'License :: OSI Approved :: Apache Software License',
@@ -23,4 +23,4 @@ setup(name="netomaton",
       packages=packages,
       keywords=["network automata", "cellular automata", "complexity", "complex systems", "computation", "non-linear dynamics"],
       python_requires='>3.5.2',
-      install_requires=["numpy >= 1.15.4", "matplotlib >= 3.0.2", "networkx == 2.2", "scipy == 1.3.1", "msgpack == 1.0.2"])
+      install_requires=["numpy >= 1.15.4", "matplotlib >= 3.0.2", "networkx >= 2.2", "scipy >= 1.3.1", "msgpack >= 1.0.2"])
