c5d5ee28b42e3de0d7018cabbd408c5b38375f58
Luis Antunes <lantunes@gmail.com>
2023-01-14 16:38:07 -0500

    bumping up version

--- netomaton/__init__.py
+++ netomaton/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 from .evolution import evolve_topology, init_simple2d, init_random, check_np, do_activity_rule, evolve_activities, \
     NodeContext, UpdateOrder, init_simple, evolve, TopologyContext, PerturbationContext, MemoizationKey
